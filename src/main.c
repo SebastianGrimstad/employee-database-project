29d90a8f7091b43008bef80c269d760b2cee750a
Sebastian Grimstad <sebastian@grimstad.dk>
2025-08-11 20:42:34 +0200

    add user flags -n -f

--- src/main.c
+++ src/main.c
@@ -8,8 +8,46 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+    printf("Usage %s -n -f <database file>\n", argv[0]);
+    printf("\t -n   -   create new database\n");
+    printf("\t -f   -   (required) path to database file\n");
+    return;
 }
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+    int c = 0;
+    bool newfile = false;
+    char *filepath = NULL;
+
+    while ((c = getopt(argc, argv, "nf:")) != -1) {
+        switch (c) {
+            case 'n':
+
+                newfile = true;
+                break;
+
+            case 'f':
+
+                filepath = optarg;
+                break;
+
+            case '?':
+                printf("Unknown flag -%c\n", c);
+                break;
+
+            default:
+                return -1;
+        }
+    }
+
+    if (filepath == NULL) {
+        printf("Filepath is a required argument\n");
+        print_usage(argv);
+        return 0;
+    }
+
+    printf("Newfile: %d\n", newfile);
+    printf("filepath: %s\n", filepath);
+
+    return 0;
 }
